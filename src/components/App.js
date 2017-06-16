d075691afc1635d35650127df59a938d23791f5c
German Gonzalez <nordicod3@gmail.com>
2017-06-15 21:52:24 -0500

    pssing status as props and state with ES6 Classes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
-import { createClass } from 'react'
-
-export const App = createClass({
-	
-	getInitialState(){
-		return{
-			allSkiDays:[
+import { Component } from 'react'
+import { SkiDayList } from './SkiDayList'
+import { SkiDayCount } from './SkiDayCount'
+export class App extends Component{
+	constructor(props) {
+		super(props)
+		this.state = {
+			allSkiDays: [
 			{
 				resort: "squaw Valley",
 				date: new Date("1/2/2016"),
@@ -25,13 +26,22 @@ export const App = createClass({
 			}
 		]
 		}
-	},
+	}
+
+	countDays(filter){
+		const {allSkiDays} = this.state
+		return allSkiDays.filter(
+			(day) => (filter) ? day[filter] : day).length
+	}
 
 	render(){
 		return (
 			<div className = "app">
-				{this.state.allSkiDays[0]["resort"]}
+				<SkiDayList days={this.state.allSkiDays}/>
+				<SkiDayCount 	total={this.countDays()}
+								powder={this.countDays("powder")}
+								backcountry={this.countDays("backcountry")}/>
 			</div>
 		)
 	}
-})
+}
