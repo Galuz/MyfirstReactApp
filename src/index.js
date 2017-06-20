87714e7f7f3e78f919809ba32da3778b474f2b3d
German Gonzalez <nordicod3@gmail.com>
2017-06-19 20:38:24 -0500

    adding React Touter, and setting up routes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import {render} from 'react-dom'
 //import {SkiDayList} from './components/SkiDayList'
 //import {SkiDayCount} from './components/SkiDayCount'
 import { App } from './components/App'
+import { Router, Route, hashHistory } from 'react-router'
+import { Whoops404 } from './components/Whoops404'
 window.React = React
 
 /*render(
@@ -11,8 +13,13 @@ window.React = React
 )*/
 
 render(
-	<App />,
-	//si pasas el array sin datos debe salir un error, si mandas un string debe salir otro error
+	<Router history={hashHistory}>
+		<Route path="/" component={App}/>
+		<Route path="list-days" component={App}/>
+		<Route path="add-day" component={App}/>
+		<Route path="*" component={Whoops404}/>
+	</Router>,
+
 
 	document.getElementById('react-container')
 )
